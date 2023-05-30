b10596a3f945acf53ca3687e0be4db01e56e0a1f
ginalsky <113460457+ginalsky@users.noreply.github.com>
2023-05-30 13:46:20 +0200

    Add files via upload

---
+++ CurrencyCalculator_MVC/src/main/java/pl/javastart/currencycalc/ExchangeController.java
@@ -0,0 +1,61 @@
+package pl.javastart.currencycalc;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+@WebServlet("/convert")
+public class ExchangeController extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (validateParameters(request)) {
+            successDispatch(request, response);
+        } else {
+            errorDispatch(request, response);
+        }
+    }
+
+    private boolean validateParameters(HttpServletRequest request) {
+        String exchangeParam = request.getParameter("exchange");
+        if (exchangeParam == null || exchangeParam.equals("")) {
+            request.setAttribute("message", "Typ wymiany nie został określony");
+            return false;
+        } else {
+            ExchangeType[] exchangeTypes = ExchangeType.values();
+            boolean invalidExchangeType = Arrays.stream(exchangeTypes)
+                    .map(Enum::toString)
+                    .noneMatch(exchangeType -> exchangeType.equals(exchangeParam));
+            if (invalidExchangeType) {
+                request.setAttribute("message", "Nieznany typ wymiany: " + exchangeParam);
+                return false;
+            }
+        }
+        String value = request.getParameter("value");
+        if (value == null || value.equals("")) {
+            request.setAttribute("message", "Kwota nie została określona");
+            return false;
+        } else if (!value.matches("-?\\d+(\\.\\d+)?")) {
+            request.setAttribute("message", "Nieprawidłowy numerek");
+            return false;
+        }
+        return true;
+    }
+
+    private void successDispatch(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        BigDecimal value = new BigDecimal(request.getParameter("value"));
+        ExchangeType exchangeType = ExchangeType.valueOf(request.getParameter("exchange"));
+        ExchangeResult exchangeResult = ExchangeCalculator.exchange(value, exchangeType);
+        request.setAttribute("result", exchangeResult);
+        request.getRequestDispatcher("/result.jsp").forward(request, response);
+    }
+
+    private void errorDispatch(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/error.jsp").forward(request, response);
+    }
+}
